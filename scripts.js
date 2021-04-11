8cd43683b6ffb178591964216c02c554197e33ad
lydia-254 <lydia.wangare@student.moringaschool.com>
2021-04-11 12:06:53 +0300

    Add link script.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,9 @@ let nameOfDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Frida
 let nameOfMales  = ["Kwasi", "Kwado","Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 let nameOfFemales = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 
+function getDetails(){
+    let year = parseInt(document.getElementById("year").value);
+    let month = parseInt(document.getElementById("month").value);
+    let day = parseInt(document.getElementById("day").value);
+    console.log(year, month, day)
+}
