f92afca6ec743499a3e8a40aee31f4e4911fd652
lydia-254 <lydia.wangare@student.moringaschool.com>
2021-04-11 11:51:18 +0300

    Add script.js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+let nameOfDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let nameOfMales  = ["Kwasi", "Kwado","Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+let nameOfFemales = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
