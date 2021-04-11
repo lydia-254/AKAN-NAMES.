e85cc123a85a39b9d7cf66a5131d2c14347fec5a
lydia-254 <lydia.wangare@student.moringaschool.com>
2021-04-11 12:20:41 +0300

    Add changes on getDetails

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ let nameOfFemales = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 function getDetails(){
     let year = parseInt(document.getElementById("year").value);
     let month = parseInt(document.getElementById("month").value);
-    var date = parseInt(document.getElementById("date").value);
-    var gender = document.getElementById("gender").value;
+    let date = parseInt(document.getElementById("date").value);
+    let gender = document.getElementById("gender").value;
     console.log(year, month, date, gender)
 }
