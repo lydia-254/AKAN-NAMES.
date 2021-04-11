8edaf03a8c7ead2a314b9fc780214c1ce64acb5d
lydia-254 <lydia.wangare@student.moringaschool.com>
2021-04-11 12:45:52 +0300

    Add date object

--- scripts.js
+++ scripts.js
@@ -7,11 +7,13 @@ function getDetails(){
     let month = parseInt(document.getElementById("month").value);
     let date = parseInt(document.getElementById("date").value);
     let gender = document.getElementById("gender").value;
-    console.log(year, month, date, gender)
+    //console.log(year, month, date, gender)
     if (month < 1 || month > 12) {
         alert("invalid month");
     }
     if (year < 1900 || year > 2029) {
         alert("invalid year")
     }
+    let d = new Date(year, month, date);
+    console.log(d)
 }
